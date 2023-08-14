65b177fa45ece883d785222c1e9553877caf8800
Denny Shu <denny.shu99@gmail.com>
2023-08-14 20:06:42 +1200

    add search feature

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBookBookmark } from '@fortawesome/free-solid-svg-icons';
@@ -6,7 +6,6 @@ import image1 from '../assets/images/Pricing Cards.png';
 import image2 from '../assets/images/Product Modal.png';
 import image3 from '../assets/images/Subscribe Card.png';
 import image4 from '../assets/images/details.png';
-import Search from '../components/Search';
 
 const NewTabLink = ({ to, children }) => (
   <Link
@@ -38,38 +37,87 @@ function ProjectItems(props) {
   );
 }
 
+function Search({ searchTerm, setSearchTerm }) {
+  return (
+    <div className='flex flex-col justify-between mt-10 px-2 md:flex-row'>
+      <div className='flex self-center space-x-2 justify-between'>
+        <input
+          type='text'
+          className='px-2 border-b-2 border-b-[#9CA3A6] bg-[#CCD4D9] focus:outline-none'
+          placeholder='Search..'
+          value={searchTerm}
+          onChange={e => setSearchTerm(e.target.value)}
+        />
+      </div>
+      <button className='p-4 mt-8 bg-[#545959] border text-[#CCD4D9] rounded-lg hover:bg-[#CCD4D9] transistion-all duration-150 hover:border-[#545959] hover:text-[#545959] md:mt-0'>
+        Upload
+      </button>
+    </div>
+  );
+}
+
 function Home() {
+  const [searchTerm, setSearchTerm] = useState('');
+
+  const projects = [
+    {
+      id: 1,
+      image: image4,
+      name: 'Product Details',
+      link: 'https://haisenberg98.github.io/denny-mini-cart-project/',
+      description: 'React Project',
+    },
+    {
+      id: 2,
+      image: image1,
+      name: 'Pricing Cards',
+      link: 'https://haisenberg98.github.io/pricing-cards/',
+      description: 'TailwindCSS Project',
+    },
+    {
+      id: 3,
+      image: image2,
+      name: 'Product Modal',
+      link: 'https://haisenberg98.github.io/product-modal/',
+      description: 'TailwindCSS Project',
+    },
+    {
+      id: 4,
+      image: image3,
+      name: 'Subscribe Card',
+      link: 'https://haisenberg98.github.io/subscribe-card/',
+      description: 'TailwindCSS Project',
+    },
+  ];
+
+  const projectsToDisplay = searchTerm
+    ? projects.filter(
+        project =>
+          project.name.toLowerCase().includes(searchTerm.toLowerCase()) ||
+          project.description.toLowerCase().includes(searchTerm.toLowerCase())
+      )
+    : projects;
   return (
     <>
-      <Search />
+      <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
       {/* Main */}
-      <main className='grid md:grid-cols-2 gap-4 justify-center mt-8'>
-        {/* Projects */}
-        <ProjectItems
-          image={image4}
-          link={'https://haisenberg98.github.io/denny-mini-cart-project/'}
-          projectName='Product Details'
-          projectDesc='React Project'
-        />
-        <ProjectItems
-          image={image1}
-          link={'https://haisenberg98.github.io/pricing-cards/'}
-          projectName='Pricing Cards'
-          projectDesc='TailwindCSS Project'
-        />
-        <ProjectItems
-          image={image2}
-          link={'https://haisenberg98.github.io/product-modal/'}
-          projectName='Product Modal'
-          projectDesc='TailwindCSS Project'
-        />
-        <ProjectItems
-          image={image3}
-          link={'https://haisenberg98.github.io/subscribe-card/'}
-          projectName='Subscribe Card'
-          projectDesc='TailwindCSS Project'
-        />
-      </main>
+      {/* Projects */}
+      {projectsToDisplay.length > 0 ? (
+        <main className='grid md:grid-cols-2 gap-4 justify-center mt-8'>
+          {projectsToDisplay.map(project => (
+            <ProjectItems
+              image={project.image}
+              link={project.link}
+              projectName={project.name}
+              projectDesc={project.description}
+            />
+          ))}
+        </main>
+      ) : (
+        <div className='flex justify-center mt-16 min-h-screen'>
+          No project found.
+        </div>
+      )}
     </>
   );
 }
